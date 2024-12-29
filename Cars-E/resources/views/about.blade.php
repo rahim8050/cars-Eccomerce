b0866e4141c598f575718028ae1ea907d00beb1d
rahim8050 <rahimranxx8050@gmail.com>
2024-12-29 12:41:39 +0000

    update

---
+++ Cars-E/resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/sass/app.scss','resources/js/app.js'])
+    <title>about</title>
+</head>
+<body>
+    <h2 class="h2">Welcome to Laravel</h2>
+    <p class="text-success">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam esse ducimus, porro tempore asperiores temporibus soluta ipsum optio error itaque ad dolorum doloremque quas eos nulla dolores suscipit repellat voluptate!</p>
+</body>
+</html>
