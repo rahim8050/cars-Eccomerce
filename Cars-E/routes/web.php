0a058aa2e56bc7f2ddf744c4b3aafa258c87e3c7
rahim8050 <rahimranxx8050@gmail.com>
2024-12-30 07:57:47 +0000

    updates

--- Cars-E/routes/web.php
+++ Cars-E/routes/web.php
@@ -1,13 +1,29 @@
 <?php
 
+use App\Http\Controllers\CarController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+   
+    // $person =[
+    //     'name'=> 'John Doe',
+    //     'age'=> 25,
+    //     'city'=> 'Nairobi',
+    // ];
+    // dd($person);
     return view('welcome');
 });
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('contact');
+Route::fallback(function () {
+    return view('404');
+})->name('404');
+Route::get('/sum{a}/{b}', function (float $a, float $b) {
+    return  $a + $b;
+})->whereNumber(['a', 'b']);
+Route::get('/cars', [CarController::class, 'car']);
+ 
